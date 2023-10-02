8165432142086d5ede925718b4e3f7cdbd71f73b
Batbaatar Tuya <tbatbaatar@gmail.com>
2023-10-02 13:42:26 +0800

    First add all files the bootstrap test

---
+++ test.js
@@ -0,0 +1,33 @@
+// q: create customer register form in html and js
+// q: create a customer object with name, email, phone, address, city, state, country, zip
+// q: create a customer array with 3 customers
+// q: create a function to display customer details in a table
+// q: create a function to add a new customer to customer array
+// q: create a function to delete a customer from customer array
+// q: create a function to update a customer from customer array
+// q: create a function to search a customer from customer array
+// q: create a function to sort customers by name, email, phone, city, state, country, zip
+// q: create a function to filter customers by name, email, phone, city, state, country, zip
+// q: create a function to validate customer form
+// q: create a function to reset customer form
+// q: create a function to load customer form
+// q: create a function to load customer table
+// q: create a function to load customer form and table
+// q: create a function to load customer form or table based on user selection
+// q: create a function to load customer form and table based on user selection
+// q: create a function to load customer form and table based on user selection
+
+
+// add bootstrap css
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</link>
+
+
+
+
+
+
+
+
+
+
